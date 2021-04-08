95c4fd6c7d8254ec1a66d524304457de0b63a8b3
Rachael <74115477+rachaeltc@users.noreply.github.com>
2021-04-08 09:52:44 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, redirect, url_for, render_template, request
+import pymysql
+
+app = Flask(__name__)
+
+connection = pymysql.connect(host='localhost', user='root', password='SQLchael324!',
+        db='genshin', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/data", methods=['POST', 'GET'])
+def data():
+    if request.method == "POST":
+        tbl = request.form["table"]
+        cur = connection.cursor()
+        query = "SELECT * FROM " + tbl 
+        cur.execute(query)
+        output2 = cur.fetchall()
+        cur.close()
+        return render_template("data.html", output = output2)
+    return render_template("data.html", output = '')
+
+@app.route("/make_team")
+def make_team():
+    return "make team page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+connection.close()
